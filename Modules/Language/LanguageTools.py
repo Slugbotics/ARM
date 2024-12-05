f2ab4d6a9aa65a287564c4a79042cbcbed365314
Aaron B <dbrunckh@ucsc.edu>
2024-12-05 13:46:25 -0800

    Added a tool for the LLM to get the current target.

--- Modules/Language/LanguageTools.py
+++ Modules/Language/LanguageTools.py
@@ -15,9 +15,13 @@ def get_visible_objects_detailed() -> list[str]:
     currently_visible_objects: list[str] = controller_getter().get_visible_object_labels_detailed()
     return currently_visible_objects
 
-@tool("Makes the arm look at the largest object with the specified label, will return false if the arm cannot perceive any objects with that label", label="The label of the object to look at")
+@tool("Makes the arm look at the largest object with the specified label, you must only give a label as returned by get_visible_objects(), will return false if the arm cannot perceive any objects with that label", label="The label of the object to look at")
 def set_look_at_target_label(label: str) -> bool:
     return controller_getter().set_target_label(label)
+
+@tool("Returns the label of the object that the arm is currently looking at")
+def get_look_at_target_label() -> str:
+    return controller_getter().get_target_label()
 
 def register_controler_tools(interpreter: LanguageInterpreter, new_controller_getter: Callable[[None], Controller]):
     """Registers tools that require a controller to be set"""
@@ -26,6 +30,7 @@ def register_controler_tools(interpreter: LanguageInterpreter, new_controller_ge
     interpreter.add_tool(get_visible_objects)
     interpreter.add_tool(get_visible_objects_detailed)
     interpreter.add_tool(set_look_at_target_label)
+    interpreter.add_tool(get_look_at_target_label)
 
 @tool("Makes an HTTP GET request to the specified URL, returning the full body of the response as a string",
       url="The URL to send the GET request to")
